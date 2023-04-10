ae05e06771cd445f5f2bc086f6d133045fe63a07
fisherbyu <fisherandrew777@gmail.com>
2023-04-10 10:33:27 -0600

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BYU_EGYPT_INTEX.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+

c3e97bf5283fd16a8be29b77b2e8587ddf8f6409
fisherbyu <fisherandrew777@gmail.com>
2023-04-13 10:36:40 -0600

    Test git

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,6 +30,7 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         return View();
+        //Test Git again
     }
     
     public IActionResult BurialData(string filter, int pageNum = 1)

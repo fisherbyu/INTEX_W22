95af84268c1123d6c1392021dff487695738681e
wanchaichab <wanchaichab@gmail.com>
2023-04-14 13:59:44 -0600

    Add Public default role. User role elevated

---
+++ Migrations/20230414193153_AddRole.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BYU_EGYPT_INTEX.Migrations
+{
+    public partial class AddRole : Migration
+    {
+        private string PublicRoleId = Guid.NewGuid().ToString();
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SeedRolesSQL(migrationBuilder);
+        }
+
+        private void SeedRolesSQL(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[,]
+                {
+                    {PublicRoleId, "Public", "PUBLIC", null}
+                });
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
